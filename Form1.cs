39b70a80f2826646a603e199640530a138166c9e
amelkin-sa <amelkin-sa@rcbi.pro>
2022-04-12 13:49:21 +0300

    add check paths

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace TwoThreadWriteApp
@@ -6,6 +8,7 @@ namespace TwoThreadWriteApp
     public partial class Form1 : Form
     {
         private readonly TwoThreadReadWriteHandler _readWriteHandler;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,8 +16,39 @@ namespace TwoThreadWriteApp
         }
 
         private void button1_Click(object sender, EventArgs e)
-        { 
-           _readWriteHandler.Copy(textBox1.Text, textBox2.Text);
+        {
+            var source = textBox1.Text;
+            var target = textBox2.Text;
+
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(target))
+            {
+                MessageBox.Show("Не заполнен источник или приёмник", "Ошибка");
+                return;
+            }
+
+            if (IsValidPath(source) is false || IsValidPath(target) is false)
+            {
+                MessageBox.Show("Указан неправильный путь", "Ошибка");
+                return;
+            }
+
+            _readWriteHandler.Copy(textBox1.Text, textBox2.Text);
+        }
+
+        private bool IsValidPath(string path)
+        {
+            Regex driveCheck = new Regex(@"^[a-zA-Z]:\\$");
+            if (!driveCheck.IsMatch(path.Substring(0, 3))) return false;
+            string strTheseAreInvalidFileNameChars = new string(Path.GetInvalidPathChars());
+            strTheseAreInvalidFileNameChars += @":/?*" + "\"";
+            Regex containsABadCharacter = new Regex("[" + Regex.Escape(strTheseAreInvalidFileNameChars) + "]");
+            if (containsABadCharacter.IsMatch(path.Substring(3, path.Length - 3)))
+                return false;
+
+            DirectoryInfo dir = new DirectoryInfo(Path.GetFullPath(path));
+            if (!dir.Exists)
+                dir.Create();
+            return true;
         }
     }
 }
